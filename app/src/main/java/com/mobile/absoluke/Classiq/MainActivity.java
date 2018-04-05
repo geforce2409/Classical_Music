6fc3b166b7831914d81781bcbfbc26933fdb03ee
QuanDao <daonguyenquan@hotmail.com>
2018-04-06 00:12:08 +0700

    badge

--- app/src/main/java/com/mobile/absoluke/Classiq/MainActivity.java
+++ app/src/main/java/com/mobile/absoluke/Classiq/MainActivity.java
@@ -1,16 +1,10 @@
 package com.mobile.absoluke.Classiq;
 
-import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -230,7 +224,6 @@ public class MainActivity extends AppCompatActivity {
         recyvwPosts.setAdapter(adapter);
     }
 }
-
 
 ////        notifyRef = mDatabase.child("notifications").child(currentUser.getUid());
 ////        notifyRef.orderByKey().limitToLast(1).addValueEventListener(new ValueEventListener() {
