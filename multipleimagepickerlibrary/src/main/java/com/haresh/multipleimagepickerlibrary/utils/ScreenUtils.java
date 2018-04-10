0f5be8c9957e897662a00f700f09dc6ef1c6b65b
QuanDao <daonguyenquan@hotmail.com>
2018-04-10 22:28:33 +0700

    some fixes

--- multipleimagepickerlibrary/src/main/java/com/haresh/multipleimagepickerlibrary/utils/ScreenUtils.java
+++ multipleimagepickerlibrary/src/main/java/com/haresh/multipleimagepickerlibrary/utils/ScreenUtils.java
@@ -2,7 +2,6 @@ package com.haresh.multipleimagepickerlibrary.utils;
 
 import android.content.Context;
 import android.graphics.Point;
-import android.os.Build;
 import android.view.Display;
 import android.view.WindowManager;
 
@@ -13,13 +12,7 @@ public class ScreenUtils {
         WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         Display display = wm.getDefaultDisplay();
         Point out = new Point();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            display.getSize(out);
-        } else {
-            int width = display.getWidth();
-            int height = display.getHeight();
-            out.set(width, height);
-        }
+        display.getSize(out);
         return out;
     }
 }
